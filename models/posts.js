de7b6d22158203781670ac775527ea95b6128e12
rawriie <crystaxn@proton.me>
2026-02-28 20:11:20 +0700

    i forgot what i updated

--- models/posts.js
+++ models/posts.js
@@ -1,38 +1,43 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const postSchema = new mongoose.Schema({
-  userId:{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Users',
-    required: true
+const postSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Users",
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    imgPath: {
+      type: String,
+      required: false,
+    },
+    parentId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Posts",
+    },
   },
-  content:{
-    type: String,
-    required: true
-  },
-  parentId:{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Posts'
-  },
-  
-  
-}, {timestamps: true})
+  { timestamps: true },
+);
 
-postSchema.virtual('likesCount', {
-  ref: 'Likes',
-  localField: '_id',
-  foreignField: 'postId',
-  count: true
+postSchema.virtual("likesCount", {
+  ref: "Likes",
+  localField: "_id",
+  foreignField: "postId",
+  count: true,
 });
 
-postSchema.virtual('replyCount', {
-  ref: 'Posts',
-  localField: '_id',
-  foreignField: 'parentId',
-  count: true
+postSchema.virtual("replyCount", {
+  ref: "Posts",
+  localField: "_id",
+  foreignField: "parentId",
+  count: true,
 });
 
-postSchema.set('toJSON', { virtuals: true });
-postSchema.set('toObject', { virtuals: true });
+postSchema.set("toJSON", { virtuals: true });
+postSchema.set("toObject", { virtuals: true });
 
-module.exports = mongoose.model('Posts', postSchema)
+module.exports = mongoose.model("Posts", postSchema);
--- routes/posts.js
+++ routes/posts.js
@@ -1,271 +1,281 @@
-const express = require('express');
-const router = express.Router()
-const Posts = require('../models/posts');
-const Likes = require('../models/likes')
-const { protect } = require('../middleware/users');
-const Users = require('../models/users');
-const likes = require('../models/likes');
-
+const express = require("express");
+const router = express.Router();
+const Posts = require("../models/posts");
+const Likes = require("../models/likes");
+const { protect } = require("../middleware/users");
+const Users = require("../models/users");
+const likes = require("../models/likes");
+const { upload, uploadToCloudinary } = require("../middleware/upload");
 
 //Get all posts
-router.get('/', protect, async (req, res) => {
+router.get("/", protect, async (req, res) => {
   try {
-    const posts = await Posts.find().populate('userId', 'username displayName profilePic').populate('likesCount').populate('replyCount').sort({ createdAt: -1 }).lean()
+    const posts = await Posts.find()
+      .populate("userId", "username displayName profilePic")
+      .populate("likesCount")
+      .populate("replyCount")
+      .sort({ createdAt: -1 })
+      .lean();
 
-    const postIds = posts.map(p => p._id);
+    const postIds = posts.map((p) => p._id);
     const likedPosts = await Likes.find({
       userId: req.user._id,
-      postId: { $in: postIds }
-    }).select('postId');
+      postId: { $in: postIds },
+    }).select("postId");
 
-    const likedPostIds = likedPosts.map(l => l.postId.toString());
+    const likedPostIds = likedPosts.map((l) => l.postId.toString());
 
     for (let i = 0; i < posts.length; i++) {
       posts[i].isLikedByMe = likedPostIds.includes(posts[i]._id.toString());
     }
 
-    res.json(posts)
+    res.json(posts);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
-  catch (err) {
-    res.status(500).json({ message: err.message })
-  }
-})
+});
 
 //Get posts by id
-router.get('/:id', protect, getPost, (req, res) => {
-  res.json(res.post)
-})
+router.get("/:id", protect, getPost, (req, res) => {
+  res.json(res.post);
+});
 
 //Create new post
-router.post('/', protect, async (req, res) => {
+router.post("/", protect, upload.single("uploadedFile"), async (req, res) => {
   try {
+    const img = await uploadToCloudinary(req.file.buffer, false);
     if (req.body.parentId) {
       const post = new Posts({
         userId: req.user._id,
         content: req.body.content,
-        parentId: req.body.parentId
+        parentId: req.body.parentId,
+        imgPath: img,
+      });
 
-      })
-
-      const newPost = await post.save()
-      res.status(201).json(newPost)
-    }
-    else{
+      const newPost = await post.save();
+      res.status(201).json(newPost);
+    } else {
       const post = new Posts({
         userId: req.user._id,
         content: req.body.content,
+        imgPath: img,
+      });
 
-      })
-
-      const newPost = await post.save()
-      res.status(201).json(newPost)
+      const newPost = await post.save();
+      res.status(201).json(newPost);
     }
-
-    
+  } catch (err) {
+    res.status(400).json({ message: err.message });
   }
-  catch (err) {
-    res.status(400).json({ message: err.message })
-  }
-})
-
+});
 
 //Edit post
-router.patch('/:id', protect, getPost, async (req, res) => {
-  console.log('BODY IS:', req.body)
+router.patch("/:id", protect, getPost, async (req, res) => {
+  console.log("BODY IS:", req.body);
   if (req.body.content == null) {
     return res.status(400).json({
-      message: "content is required."
-    })
+      message: "content is required.",
+    });
   }
 
-  res.post.content = req.body.content
+  res.post.content = req.body.content;
 
   try {
     if (req.user._id.equals(res.post.userId._id)) {
-      const updatedPost = await res.post.save()
+      const updatedPost = await res.post.save();
       res.status(200).json({
         message: "Post updated.",
-        post: updatedPost
-      })
-    }
-    else {
-      return res.status(401).json({ "message": `User not authorized.` })
+        post: updatedPost,
+      });
+    } else {
+      return res.status(401).json({ message: `User not authorized.` });
     }
   } catch (err) {
-    res.status(400).json({ message: err.message })
+    res.status(400).json({ message: err.message });
   }
-})
-
+});
 
 //Delete post
-router.delete('/:id', protect, getPost, async (req, res) => {
+router.delete("/:id", protect, getPost, async (req, res) => {
   try {
     if (req.user._id.equals(res.post.userId._id)) {
-      await Posts.deleteOne({ _id: req.params.id })
-      return res.status(200).json({ "message": "Deleted post." })
-    }
-    else {
-      return res.status(401).json({ "message": `User not authorized.` })
+      await Posts.deleteOne({ _id: req.params.id });
+      return res.status(200).json({ message: "Deleted post." });
+    } else {
+      return res.status(401).json({ message: `User not authorized.` });
     }
   } catch (err) {
-    res.status(500).json({ "message": err.message })
-
+    res.status(500).json({ message: err.message });
   }
-})
+});
 
 //Add Like to a post
-router.post('/like/:postId', protect, async (req, res) => {
-  try{
-    const likeExists = await Likes.findOne({userId: req.user._id, postId: req.params.postId});
+router.post("/like/:postId", protect, async (req, res) => {
+  try {
+    const likeExists = await Likes.findOne({
+      userId: req.user._id,
+      postId: req.params.postId,
+    });
 
-    if(likeExists){
-      
-      return res.status(400).json({message: "Post already liked."});
-      
+    if (likeExists) {
+      return res.status(400).json({ message: "Post already liked." });
     }
     const like = new Likes({
       userId: req.user._id,
-      postId: req.params.postId
+      postId: req.params.postId,
     });
 
     const newLike = await like.save();
     res.status(201).json(newLike);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
-  catch(err){
-    res.status(500).json({ "message": err.message })
-  }
-})
+});
 
 //Remove Like from a post
-router.delete('/like/:postId', protect, async (req, res) => {
-  try{
-  
-    const like = await Likes.findOne({userId: req.user._id, postId: req.params.postId});
+router.delete("/like/:postId", protect, async (req, res) => {
+  try {
+    const like = await Likes.findOne({
+      userId: req.user._id,
+      postId: req.params.postId,
+    });
 
-    if(!like){
-      return res.status(404).json({message: "Like not found."});
+    if (!like) {
+      return res.status(404).json({ message: "Like not found." });
     }
 
     await like.deleteOne();
-    res.status(200).json({message: "Removed like."});
+    res.status(200).json({ message: "Removed like." });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
-  catch(err){
-    res.status(500).json({ "message": err.message })
-  }
-})
+});
 
 //Get posts by userId
-router.get('/user/posts/:userId', protect, async (req, res) => {
+router.get("/user/posts/:userId", protect, async (req, res) => {
   try {
-    const user = await Users.findById(req.params.userId).select('-password -email');
+    const user = await Users.findById(req.params.userId).select(
+      "-password -email",
+    );
 
     if (!user) {
       return res.status(404).json({ message: "User not found." });
     }
 
-    const posts = await Posts.find({ userId: req.params.userId }).populate('userId', 'username displayName profilePic').populate('likesCount').populate('replyCount').sort({ createdAt: -1 }).lean()
-   
-    const postIds = posts.map(p => p._id);
+    const posts = await Posts.find({ userId: req.params.userId })
+      .populate("userId", "username displayName profilePic")
+      .populate("likesCount")
+      .populate("replyCount")
+      .sort({ createdAt: -1 })
+      .lean();
+
+    const postIds = posts.map((p) => p._id);
     const likedPosts = await Likes.find({
       userId: req.user._id,
-      postId: { $in: postIds }
-    }).select('postId');
+      postId: { $in: postIds },
+    }).select("postId");
 
-    const likedPostIds = likedPosts.map(l => l.postId.toString());
+    const likedPostIds = likedPosts.map((l) => l.postId.toString());
 
     for (let i = 0; i < posts.length; i++) {
       posts[i].isLikedByMe = likedPostIds.includes(posts[i]._id.toString());
     }
 
-    res.json(posts)
+    res.json(posts);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
-  catch (err) {
-    res.status(500).json({ "message": err.message })
-  }
-})
+});
 
 //Get post replies
-router.get('/user/:id', protect, async (req, res) => {
+router.get("/user/:id", protect, async (req, res) => {
   try {
-    const replies = await Posts.find({ parentId: req.params.id }).populate('userId', 'username displayName profilePic').populate('likesCount').populate('replyCount').sort({ createdAt: -1 }).lean();
-    
-    const replyIds = replies.map(p => p._id);
+    const replies = await Posts.find({ parentId: req.params.id })
+      .populate("userId", "username displayName profilePic")
+      .populate("likesCount")
+      .populate("replyCount")
+      .sort({ createdAt: -1 })
+      .lean();
+
+    const replyIds = replies.map((p) => p._id);
     const likedPosts = await Likes.find({
       userId: req.user._id,
-      postId: { $in: replyIds }
-    }).select('postId');
+      postId: { $in: replyIds },
+    }).select("postId");
 
-    const likedPostIds = likedPosts.map(l => l.postId.toString());
+    const likedPostIds = likedPosts.map((l) => l.postId.toString());
 
     for (let i = 0; i < replies.length; i++) {
       replies[i].isLikedByMe = likedPostIds.includes(replies[i]._id.toString());
     }
     res.json(replies);
-    
-
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
-  catch (err) {
-    res.status(500).json({ "message": err.message })
-  }
-})
+});
 
 //Get all replies by user
-router.get('/user/replies/:userId',protect , async (req, res) => {
+router.get("/user/replies/:userId", protect, async (req, res) => {
   try {
-    const user = await Users.findById(req.params.userId).select('-password -email');
+    const user = await Users.findById(req.params.userId).select(
+      "-password -email",
+    );
 
     if (!user) {
       return res.status(404).json({ message: "User not found." });
     }
 
-    const replies = await Posts.find({ parentId: { $ne: null, $exists: true }, userId: req.params.userId }).populate('userId', 'username displayName profilePic').populate('likesCount').populate('replyCount').sort({ createdAt: -1 }).lean();
+    const replies = await Posts.find({
+      parentId: { $ne: null, $exists: true },
+      userId: req.params.userId,
+    })
+      .populate("userId", "username displayName profilePic")
+      .populate("likesCount")
+      .populate("replyCount")
+      .sort({ createdAt: -1 })
+      .lean();
 
-    const replyIds = replies.map(p => p._id);
+    const replyIds = replies.map((p) => p._id);
     const likedPosts = await Likes.find({
       userId: req.user._id,
-      postId: { $in: replyIds }
-    }).select('postId');
+      postId: { $in: replyIds },
+    }).select("postId");
 
-    const likedPostIds = likedPosts.map(l => l.postId.toString());
+    const likedPostIds = likedPosts.map((l) => l.postId.toString());
 
     for (let i = 0; i < replies.length; i++) {
       replies[i].isLikedByMe = likedPostIds.includes(replies[i]._id.toString());
     }
-    res.json(replies)
+    res.json(replies);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
-  catch (err) {
-    res.status(500).json({ "message": err.message })
-  }
-})
+});
 
 async function getPost(req, res, next) {
   let post;
   try {
-    post = await Posts.findById(req.params.id).populate('userId', 'username displayName profilePic').populate('likesCount').lean()
-    
-   
-    
+    post = await Posts.findById(req.params.id)
+      .populate("userId", "username displayName profilePic")
+      .populate("likesCount")
+      .lean();
+
     if (post === null) {
-      return res.status(404).json({ "message": "Post not found." })
+      return res.status(404).json({ message: "Post not found." });
     }
 
-   
     const likedPost = await Likes.find({
       userId: req.user._id,
-      postId: post._id
-    }).select('postId');
+      postId: post._id,
+    }).select("postId");
 
-   
-    if(likedPost){
-      post.isLikedByMe = true
+    if (likedPost) {
+      post.isLikedByMe = true;
     }
-    
-
+  } catch (error) {
+    return res.status(500).json({ message: "Server error." });
   }
-  catch (error) {
-    return res.status(500).json({ "message": "Server error." });
-  }
-  res.post = post
-  next()
+  res.post = post;
+  next();
 }
 module.exports = router;
